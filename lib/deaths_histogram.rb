b750edde448840e424cbb925e5b50e41079ca8ce
Karla Elizabeth Alvarez Castillo <karla.alvarez.castillo@gmail.com>
2021-11-03 21:47:33 -0600

    Update Histogram class, iterate data

--- lib/deaths_histogram.rb
+++ lib/deaths_histogram.rb
@@ -1,3 +1,5 @@
-require_relative 'rick_and_morty'
+# frozen_string_literal: true
+
+require_relative "rick_and_morty"
 
 RickAndMorty::Histogram.new().deaths
--- lib/rick_and_morty.rb
+++ lib/rick_and_morty.rb
@@ -1,45 +1,37 @@
 # frozen_string_literal: true
 
 require_relative "rick_and_morty/version"
-require 'net/http'
-require 'json'
+require "net/http"
+require "json"
 
 module RickAndMorty
   class Error < StandardError; end
 
+  # Class for create Histograms
   class Histogram
     def initialize
       @base_uri = "https://rickandmortyapi.com/api/character"
-      @species = %w(Human Alien Mythological\ Creature Humanoid Robot Disease Animal Cronenberg unknow)
+      @total_characters = count_characters(get_data(@base_uri))
+
+      @count_by_specie = {}
+      @index = 1
     end
 
     def deaths
+      uri = "#{@base_uri}?status=dead"
+      iterate_pagination(uri)
+
       data = ["\nRick and Morty deaths statistics:\n"]
-      @species.each do |specie|
-        specie_data = get_data(@base_uri+"?species=#{specie}&status=dead")
-        specie_deaths = count_characters(specie_data)
-        data << "\n#{specie}: #{specie_deaths}"
-      end
-
-      total_deaths = count_characters(get_data(@base_uri+"?status=dead"))
-      total_characters = count_characters(get_data(@base_uri))
+      data << specie_histogram
+      total_deaths = count_characters(get_data(uri))
       data << "\n\nTotal deaths: #{total_deaths}"
-      data << "\nTotal characters: #{total_characters}"
-      data << "\n#{percentage(total_characters, total_deaths)}% has died\n\n"
+      data << "\nTotal characters: #{@total_characters}"
+      data << "\n#{percentage(@total_characters, total_deaths)}% has died\n\n"
 
-      data = data.join
-
-      print_histogram(data)
-      data
-
-
+      print_histogram(data.join)
     end
 
     private
-
-    def print_histogram(data)
-      puts data
-    end
 
     def get_data(uri)
       uri = URI(uri)
@@ -47,12 +39,43 @@ module RickAndMorty
       JSON.parse(request)
     end
 
-    def percentage(total, part)
-      (part*100)/total
-    end
-
     def count_characters(data)
       data["info"]["count"]
+    end
+
+    def iterate_pagination(uri)
+      bunch_data = get_data(uri)
+      pages = bunch_data["info"]["pages"]
+
+      while @index <= pages
+        count_deaths_by_specie(bunch_data["results"])
+        bunch_data = get_data(bunch_data["info"]["next"]) unless @index >= pages
+        @index += 1
+      end
+      @index = 1
+    end
+
+    def count_deaths_by_specie(data)
+      data.each do |character|
+        @count_by_specie[character["species"]] = 0 unless @count_by_specie.key?(character["species"])
+        @count_by_specie[character["species"]] += 1
+      end
+    end
+
+    def specie_histogram
+      data = []
+      @count_by_specie.each do |specie, quantity|
+        data << "\n#{specie}: #{quantity}"
+      end
+      data
+    end
+
+    def percentage(total, part)
+      (part * 100) / total
+    end
+
+    def print_histogram(data)
+      puts data
     end
   end
 end
